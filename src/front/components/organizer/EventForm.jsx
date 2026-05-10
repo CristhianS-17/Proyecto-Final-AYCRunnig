bcf69b710ed2ac396389fc75738fda6d793a6ff8
Yoan <dafneangel3008@gmail.com>
2026-05-10 00:08:43 +0000

    formulario listo yoannis

--- src/front/components/organizer/EventForm.jsx
+++ src/front/components/organizer/EventForm.jsx
@@ -1,27 +1,60 @@
+import { useState } from "react";
+
 export const EventForm = () => {
+    const [eventName, setEventName] = useState("");
+    const [location, setLocation] = useState("");
+    const [date, setDate] = useState("");
+    const [description, setDescription] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(eventName);
+        console.log(location);
+        console.log(date);
+        console.log(description);
+    };
+
     return (
         <div className="event-form">
             <h2 className="event-form-title">Create Event</h2>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label>Event Name</label>
-                    <input type="text" placeholder="Event name" />
+                    <input 
+                        type="text" 
+                        placeholder="Event name" 
+                        value={eventName}
+                        onChange={(e) => setEventName(e.target.value)}
+                    />
                 </div>
 
                  <div className="form-group">
                     <label>Location</label>
-                    <input type="text" placeholder="Event Location" />
+                    <input 
+                        type="text" 
+                        placeholder="Event Location" 
+                        value={location}
+                        onChange={(e) => setLocation(e.target.value)}
+                    />
                 </div>
 
                 <div className="form-group">
                     <label>Date</label>
-                    <input  type="date"/>
+                    <input 
+                        type="date"
+                        value={date}
+                        onChange={(e) => setDate(e.target.value)}
+                    />
                 </div>
 
                 <div className="form-group">
                     <label>Event Description</label>
-                    <textarea placeholder="Event Description"></textarea>
+                    <textarea
+                        placeholder="Event Description"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}            
+                    ></textarea>
                 </div>
 
                 <button className="create-event-button">Create Event</button>

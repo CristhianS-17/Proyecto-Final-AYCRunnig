cf6c84d83bfca8162956a1e1d2367cc0aaccf18f
Yoan <dafneangel3008@gmail.com>
2026-05-11 18:10:53 +0000

    unificando cambios

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -3,18 +3,26 @@ import logo from "../assets/runbound-logo.png";
 
 export const Navbar = () => {
     return (
-        <nav className="navbar">
-            <div className="navbar-logo">
-                <img src={logo} alt="RunBound Logo" />
-                <span className="run">Run</span><span className="bound">Bound</span>
-
-            </div>
-
-            <div className="nav-right">
-                <Link to="/map">Mapa</Link>
-                <Link to="/organizer">Organizer</Link>
-                <Link to="/admin">Admin</Link>
-                <button>Logout</button>
+        <nav className="navbar navbar-light bg-light">
+            <div className="container">
+                <Link className="navbar-logo" to="/">
+                    <img src={logo} alt="RunBound Logo" />
+                    <span className="run">Run</span><span className="bound">Bound</span>
+                </Link>
+                <div className="ml-auto">
+                    <Link to="/map">
+                        Mapa
+                    </Link>
+                    <Link to="/organizer">
+                        Organizer
+                    </Link>
+                    <Link to="/admin">
+                        Admin
+                    </Link>
+                    <button className="btn btn-danger btn-sm">
+                        Logout
+                    </button>
+                </div>
             </div>
         </nav>
     );

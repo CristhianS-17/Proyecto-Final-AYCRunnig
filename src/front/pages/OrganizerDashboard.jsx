0e7734de8d4662d20608f8721ca7d620f2413b15
Yoan <dafneangel3008@gmail.com>
2026-05-18 17:29:46 +0000

    cambios importantes

--- src/front/pages/OrganizerDashboard.jsx
+++ src/front/pages/OrganizerDashboard.jsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from "react";
 import { EventForm } from "../components/organizer/EventForm";
 
 export const OrganizerDashboard = () => {
+    const [events, setEvents] = useState([]);
+
+    const getEvents = async () => {
+
+        const response = await fetch(
+            import.meta.env.VITE_BACKEND_URL + "/events"
+        );
+
+        const data = await response.json();
+
+        setEvents(data);
+
+    };
+
+    useEffect(() => {
+        getEvents();
+    }, []);
+
     return (
         <div className="organizer-dashboard">
             <h1 className="dashboard-title">Organizer Dashboard</h1>
@@ -10,6 +29,22 @@ export const OrganizerDashboard = () => {
             </p>
 
             <EventForm />
+
+            <section className="my-events-section">
+                <h2>My Events</h2>
+
+                <p>Here you will manage your created events.</p>
+
+                {events.length === 0 && (
+                    <p>No events created yet.</p>
+                )}
+
+                {events.map((event) => (
+                    <div key={event.id}>
+                        <h3>{event.title}</h3>
+                    </div>
+                ))}
+            </section>
         </div>
     );
 };

11d6e165d401df40983783844850e3339865472c
Yoan <dafneangel3008@gmail.com>
2026-05-20 16:50:21 +0000

    Updated global navbar navigation

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -2,27 +2,47 @@ import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/runbound-logo.png";
 
 export const Navbar = () => {
+
     const navigate = useNavigate();
+
     const isLoggedIn = !!localStorage.getItem("token");
+    const role = localStorage.getItem("role");
 
     const handleLogout = () => {
+
         localStorage.removeItem("token");
         localStorage.removeItem("role");
+
         navigate("/login");
     };
 
     return (
         <nav className="navbar navbar-light bg-light">
+
             <div className="container">
+
                 <Link className="navbar-logo" to="/">
                     <img src={logo} alt="RunBound Logo" />
                 </Link>
 
                 <div className="ml-auto">
+
+                    <Link to="/">Home</Link>
+
+                    <Link to="/map">Map</Link>
+
                     {!isLoggedIn && (
-                        <Link to="/login" className="btn-beige">
-                            Login
-                        </Link>
+                        <>
+
+                            <Link to="/login" className="btn-beige">
+                                Login
+                            </Link>
+
+                            <Link to="/register">
+                                Register
+                            </Link>
+
+                        </>
                     )}
 
                     {isLoggedIn && (
@@ -32,13 +52,32 @@ export const Navbar = () => {
                             <Link to="/admin">Admin</Link>
                             <Link to="/profile">Perfil</Link>
 
-                            <button className="btn btn-danger btn-sm"onClick={handleLogout}>
+                            {role === "organizer" && (
+                                <Link to="/organizer">
+                                    Organizer
+                                </Link>
+                            )}
+
+                            {role === "admin" && (
+                                <Link to="/admin">
+                                    Admin
+                                </Link>
+                            )}
+
+                            <button
+                                className="btn btn-danger btn-sm"
+                                onClick={handleLogout}
+                            >
                                 Logout
                             </button>
+
                         </>
                     )}
+
                 </div>
+
             </div>
+
         </nav>
     );
 };
